2fc955d730d06ea4de48f0c7b8690062fc0087e2
gustavogeraldelli <geraldellig@gmail.com>
2024-03-04 01:24:46 -0300

    adding update employee functionality

--- src/main/java/me/gustavo/employeedirectory/controller/EmployeeController.java
+++ src/main/java/me/gustavo/employeedirectory/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import me.gustavo.employeedirectory.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +37,11 @@ public class EmployeeController {
     public String saveEmploee(@ModelAttribute("employee") Employee employee) {
         service.save(employee);
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/update")
+    public String updateEmployee(@RequestParam("employeeId") int id, Model model) {
+        model.addAttribute("employee", service.findById(id));
+        return "employee-form";
     }
 }
