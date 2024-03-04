e119946be926b6f8ef84f8c02282deb67f4f9b2e
gustavogeraldelli <geraldellig@gmail.com>
2024-03-04 01:08:13 -0300

    updating controller to save new employee

--- src/main/java/me/gustavo/employeedirectory/controller/EmployeeController.java
+++ src/main/java/me/gustavo/employeedirectory/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,5 +34,11 @@ public class EmployeeController {
     public String addEmployeeForm(Model model) {
         model.addAttribute("employee", new Employee());
         return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmploee(@ModelAttribute("employee") Employee employee) {
+        service.save(employee);
+        return "redirect:/employees/list";
     }
 }
